a3fa93baf9eee33b8947ce1b50864022fe3079e0
Tom Moulard <tom.moulard@epita.fr>
2021-01-27 10:24:01 +0100

    python: simpler code

--- lambdapythonfunction/lambdapythonfunction.py
+++ lambdapythonfunction/lambdapythonfunction.py
@@ -1,12 +1,3 @@
-import urllib.request
-import urllib.parse
-from os import path
-#  from jinja2 import Environment, FileSystemLoader
-from myjinja import jinja
-
-#  env = Environment(loader=FileSystemLoader(path.join(path.dirname(__file__), 'templates'), encoding='utf8'))
-env = jinja(path.join(path.dirname(__file__), 'templates'))
-
 def send(msg, status=200):
     return {
             "statusCode": status,
@@ -16,32 +7,6 @@ def send(msg, status=200):
             "body": msg
             }
 
-
-def err(msg):
-    return send(msg, 500)
-
-
-def ok(msg):
-    return send(msg)
-
-
-def html(url, size):
-    f = urllib.request.urlopen(url)
-    csv = f.read().decode('utf-8')
-    template = env.get_template('index.html', )
-    return ok(template.render(csv="\n".join(csv.split("\n")[:size]), size=size))
-
-
 def lambdapythonfunction(event, context):
-    try:
-        url = event['queryStringParameters']['url']
-        size = event['queryStringParameters']['size']
-    except:
-        template = env.get_template('error.html', )
-        return err(template.render())
-    return html(url, size)
+    return send("Hello world !")
 
-
-if __name__ == "__main__":
-    #  print(lambdapythonfunction("https://pi.moulard.org/speedtest/cronSpeedtest-cli.csv", -1))
-    print(html("https://pi.moulard.org/speedtest/cronSpeedtest-cli.csv", 2))
